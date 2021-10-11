9c54f0484306d6264f1a741d66654948ea2e6b3b
gaojuphy <1571066810@qq.com>
2021-10-11 20:20:38 +0800

    完善blog

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,51 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts"
+
+// import someDatabaseSDK from 'someDataBaseSDK'
+// const databaseClient = someDatabaseSDK.createClient(...)
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+
+  // fetch post data form an external API endpoint
+  // const res = swait fetch('..')
+  // return res.json()
+  // Next.js`fetch()`在客户端和服务器上都使用polyfill，不需要在导入它
+
+  // 直接查询数据库
+  // return databaseClient.query('SELECT posts...')
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Keep the existing code here */}
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
